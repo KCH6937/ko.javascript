e5a5c510152cb2545cf93b9d1e3876671d52a403
KCH6937 <adasddasd12@naver.com>
2022-03-14 20:59:31 +0900

    2-9 비교 연산자

---
+++ 자바스크립트 기본/비교 연산자.js
@@ -0,0 +1,9 @@
+// 과제 1
+// 비교
+5 > 4 // true
+"apple" > "pineapple" // false
+"2" > "12" // true
+undefined == null // true
+undefined === null // false
+null == "\n0\n" // false
+null === +"\n0\n" // false
