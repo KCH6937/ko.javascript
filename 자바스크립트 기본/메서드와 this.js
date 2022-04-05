4c7d662784ae9a9f8453228e86a45a06a9a95d6a
KCH6937 <adasddasd12@naver.com>
2022-04-05 22:06:05 +0900

    메서드와 this

---
+++ 자바스크립트 기본/메서드와 this.js
@@ -0,0 +1,51 @@
+// 과제 1
+// 객체 리터럴에서 this 사용하기
+function makeUser() {
+    return {
+      name: "John",
+      ref: this
+    };
+};
+  
+let user = makeUser();
+  
+alert( user.ref.name );
+  
+  
+// 과제 2
+// 계산기 만들기
+let calculator = {
+    read() {
+        this.a = +prompt('첫 번째 값:', 0);
+        this.b = +prompt('두 번째 값:', 0);
+    },
+    sum() {
+        return this.a + this.b;
+    },
+    mul() {
+        return this.a * this.b;
+    }
+
+}
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+  
+
+// 과제 3
+// 체이닝
+let ladder = {
+    step: 0,
+    up() {
+      this.step++;
+      return this;
+    },
+    down() {
+      this.step--;
+      return this;
+    },
+    showStep: function() { // 사다리에서 몇 번째 단에 올라와 있는지 보여줌
+      alert( this.step );
+    }
+};
